e7584ec97ac0d928d2293bd1aaa4885242a20227
Bill Psomas <psomasbill@gmail.com>
2024-04-23 20:10:28 +0000

    update files

--- predict.py
+++ predict.py
@@ -19,11 +19,11 @@ parser.add_argument('--imgsz', type=int, default=256, help='image size')
 parser.add_argument('--conf', type=float, default=0.05, help='confidence threshold')
 parser.add_argument('--iou', type=float, default=0.5, help='IOU threshold')
 parser.add_argument('--source', type=str, default="/mnt/data/datasets/2022_GeoAI_Martian_Challenge_Dataset/val_images", help='source directory for images')
-parser.add_argument('--save_dir', type=str, default="/mnt/data/bill/code/yolov8/runs/detect/yolov5x-mars-256-evaluation-results/", help='directory to save results')
+parser.add_argument('--save_dir', type=str, default="/mnt/data/bill/code/yolov8/runs/detect/yolov8n-mars-256-evaluation-results/", help='directory to save results')
 parser.add_argument('--json_name', type=str, default="results.json", help='name of the JSON file to save results')
 parser.add_argument('--save_imgs', action='store_true', help='flag to save images')
 parser.add_argument('--save_txts', action='store_true', help='flag to save text files')
-parser.add_argument('--model_name', type=str, default="/mnt/data/bill/code/yolov8/runs/detect/yolov5x-mars-imgsz-256-batch-128/weights/best.pt", help='path to the model weights')
+parser.add_argument('--model_name', type=str, default="/mnt/data/bill/code/yolov8/runs/detect/yolov8n-mars-imgsz-256-batch-512/weights/best.pt", help='path to the model weights')
 
 args = parser.parse_args()
 
@@ -31,10 +31,12 @@ args = parser.parse_args()
 os.makedirs(args.save_dir, exist_ok=True)
 
 if args.save_imgs:
-    os.makedirs(os.path.join(args.save_dir, 'imgs'), exist_ok=True)
+    imgs_dir = os.path.join(args.save_dir, 'imgs')
+    os.makedirs(imgs_dir, exist_ok=True)
 
 if args.save_txts:
-    os.makedirs(os.path.join(args.save_dir, 'txts'), exist_ok=True)
+    txts_dir = os.path.join(args.save_dir, 'txts')
+    os.makedirs(txts_dir, exist_ok=True)
 
 # Load a YOLOv8 model from a pre-trained weights file
 model = YOLO(args.model_name)
@@ -55,10 +57,10 @@ for res in results:
     if args.save_imgs:
         im_array = res.plot()  # plot a BGR numpy array of predictions
         im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-        im.save(os.path.join(args.save_dir, im_id + '.png'))  # save image
+        im.save(os.path.join(imgs_dir, im_id + '.png'))  # save image
 
     if args.save_txts:
-        res.save_txt(os.path.join(args.save_dir, res.path.split('/')[-1].replace('.png', '.txt'))) # save txt 
+        res.save_txt(os.path.join(txts_dir, res.path.split('/')[-1].replace('.png', '.txt'))) # save txt 
 
     # Save JSON for official COCO format evaluation
     bxs = res.boxes.xywh.cpu().detach().numpy().tolist() # export boxes to a list
--- train.py
+++ train.py
@@ -1,21 +1,52 @@
 import argparse
+import yaml
 from ultralytics import YOLO
 
 # Define the argument parser and add arguments
-parser = argparse.ArgumentParser(description='YOLOv8 Training Script')
-parser.add_argument('--model_name', type=str, default='yolov5x.pt', help='Model name or path')
-parser.add_argument('--imgsz', type=int, default=256, help='Image size')
-parser.add_argument('--conf', type=float, default=0.4, help='Confidence threshold')
-parser.add_argument('--iou', type=float, default=0.2, help='IoU threshold')
-parser.add_argument('--epochs', type=int, default=600, help='Number of epochs')
-parser.add_argument('--device', nargs='+', type=int, default=[1], help='Device IDs of GPUs to use')
-parser.add_argument('--batch', type=int, default=128, help='Batch size')
-parser.add_argument('--data', type=str, default='data/mars.yaml', help='Path to dataset')
-parser.add_argument('--name', type=str, default='yolov5x-mars-imgsz-256-batch-128', help='Name for the training session')
+def parse_args():
+    # General training arguments
+    parser = argparse.ArgumentParser(description='YOLOv8 Training Script')
+    parser.add_argument('--model_name', type=str, default='yolov8s.pt', help='Model name or path')
+    parser.add_argument('--imgsz', type=int, default=1280, help='Image size')
+    parser.add_argument('--conf', type=float, default=0.4, help='Confidence threshold')
+    parser.add_argument('--iou', type=float, default=0.2, help='IoU threshold')
+    parser.add_argument('--epochs', type=int, default=300, help='Number of epochs')
+    parser.add_argument('--device', nargs='+', type=int, default=[0, 1], help='Device IDs of GPUs to use')
+    parser.add_argument('--batch', type=int, default=2, help='Batch size')
+    parser.add_argument('--data', type=str, default='/home/bill/code/yolov4/data/mbls_0_5_6_7_8_9_10_v1.yaml', help='Path to dataset')
+    parser.add_argument('--name', type=str, default='yolov8s-p2-mbls-0-5-6-7-8-9-10-v1-ball-players-1280', help='Name for the training session')
+    # For using feature pyramids
+    parser.add_argument('--feature_pyramid', action='store_true', help='Enable additional feature pyramid levels')
+    # For tuning model
+    parser.add_argument('--tune', action='store_true', help='Tune hyperparameters')
+    parser.add_argument('--iterations', type=int, default=300, help='Maximum number of trials (hyperparameter combinations) to run')
+    # Training with best hyperparameters
+    parser.add_argument('--hyps', type=str, default=None, help='Best hyperparameters YAML (results from tuning) path')
+    args = parser.parse_args()
+    return args
 
-# Parse the arguments
-args = parser.parse_args()
+def main(): 
+    # Parse the arguments
+    args = parse_args()
 
-# Use the arguments to train the model
-model = YOLO(args.model_name)
-model.train(data=args.data, batch=args.batch, epochs=args.epochs, imgsz=args.imgsz, device=args.device, name=args.name)
+    # Use the arguments to train the model
+    if args.feature_pyramid:
+        model = YOLO(args.model_name.split('.')[0] + '-p2.yaml').load(args.model_name)
+    else:
+        model = YOLO(args.model_name)
+
+    if args.tune:
+        # Tune hyperparameters
+        model.tune(data=args.data, batch=args.batch, epochs=args.epochs, iterations=args.iterations, imgsz=args.imgsz, device=args.device, name=args.name, optimizer='AdamW', plots=False, save=False, val=False)
+    else:
+        if args.hyps:
+            # Train model using best hyperparameters 
+            with open(args.hyps, 'r') as file:
+                hyps = yaml.safe_load(file)        
+            model.train(data=args.data, batch=args.batch, epochs=args.epochs, imgsz=args.imgsz, device=args.device, name=args.name, optimizer='AdamW', **hyps)
+        else:
+            # Train model using default hyperparameters 
+            model.train(data=args.data, batch=args.batch, epochs=args.epochs, imgsz=args.imgsz, device=args.device, name=args.name)
+
+if __name__ == "__main__":
+    main()
